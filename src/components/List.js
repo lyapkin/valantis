acc501a4a9ffc3799490f56e19ee3f0039b5320e
lyapkin <lyapkin992@gmail.com>
2024-03-02 13:53:35 +0500

    .

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import Filter from './Filter';
 import Pagination from './Pagination';
-import useProductHtml from '../hooks/useProductHtml';
 import { useRequestParams } from '../hooks/useRequestParams';
 import useData from '../hooks/useData';
 
 const List = () => {
     const [page, filter, dispatch] = useRequestParams();
     const [products, loading] = useData(page-1, filter);
-    const productHtml = useProductHtml(products);
+
+    const productsHtml = products.map(item => (<li key={item.id}>
+        <div className="product">
+            <span className="product__id">id: {item.id}; </span>
+            <span className="product__name">product: {item.product}; </span>
+            <span className="product__price">price: {item.price}; </span>
+            {item.brand && <span className="product__brand">brand: {item.brand};</span>}
+        </div>
+      </li>)
+    )  ;
     
 
     return (
@@ -20,7 +28,7 @@ const List = () => {
                 <Pagination page={page} dispatch={dispatch} />
                 Товары:
                 <ol>
-                    {loading ?  "Loading..." : productHtml}
+                    {loading ?  "Loading..." : productsHtml}
                 </ol>
             </div>
             <div className='footer'>
